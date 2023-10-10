911f654d91ce40780f0c3e7b746c0bc79d320c2c
z93478462 <1119716380@qq.com>
2023-10-10 16:45:21 +0800

    Build at 2023-10-10 16:45:20

--- customize/js/ZYDark.js
+++ customize/js/ZYDark.js
@@ -1,21 +1,4 @@
 /**
- * 钟意工具类
- */
-class ZYUtil {
-    /**
-     * 判断是否主页类
-     */
-    isIndexs = () => {
-        const urls = window.location.href.split('/');
-        const indexs = ["", "categories", "tags", "archives", "notes"]
-        if (indexs.indexOf(urls[3]) !== -1) {
-            return true;
-        } else if (indexs.indexOf("wiki") && (urls[4] === "tags" || urls[4] === "")) {
-            return true;
-        } else return false;
-    }
-}
-/**
  * 监听系统主题
  * @type {MediaQueryList}
  */
